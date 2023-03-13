cacace942205f6b724f708b5136831ab4d30b113
50NiC613 <yulianmunoz157@gmail.com>
2023-03-12 20:08:54 -0400

    instalado en vscode los plugins y configuraciones

--- routes/products.js
+++ routes/products.js
@@ -1,61 +1,62 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {Product} = require('../models/product');
-
+const { Product } = require("../models/product");
 
 /* GET Listar los productos */
-router.get('/', async function(req, res) {
-    let products = await Product.find()
-    if (!products.length) {
-        res.status(500).json({success:false});
-    }
-    res.send(products);
+router.get("/", async function (req, res) {
+  let products = await Product.find();
+  if (!products.length) {
+    res.status(500).json({ success: false });
+  }
+  res.send(products);
 });
 /* GET producto por ID */
-router.get('/:id', function(req, res) {
-    let id = req.params.id;
-    let product = products.find(product => product.id === id);
-    res.send(product);
-})
+router.get("/:id", function (req, res) {
+  let id = req.params.id;
+  let product = products.find((product) => product.id === id);
+  res.send(product);
+});
 // importar modelo de producto
 
 /* POST Crear un nuevo producto */
-router.post('/', function(req, res) {
-    let product = new Product({
-        name : req.body.name,
-        price : req.body.price,
-        description : req.body.description,
-        image : req.body.image})
-    product.save(product)
-        .then(() =>{
-        res.status(200).json({
-            message : 'Product agregado',
-            product: product
-        })
+router.post("/", function (req, res) {
+  let product = new Product({
+    name: req.body.name,
+    price: req.body.price,
+    description: req.body.description,
+    image: req.body.image,
+  });
+  product
+    .save(product)
+    .then(() => {
+      res.status(200).json({
+        message: "Product agregado",
+        product: product,
+      });
     })
-        .catch((err) =>{
-            res.status(500).json({
-                message : err.message,
-                error : err,
-            })
-        })
-})
+    .catch((err) => {
+      res.status(500).json({
+        message: err.message,
+        error: err,
+      });
+    });
+});
 /* PUT Actualizar un producto */
-router.put('/:id', function(req, res) {
-    let id = req.params.id;
-    let product = products.find(product => product.id === id);
-    product.name = req.body.name;
-    product.price = req.body.price;
-    product.description = req.body.description;
-    product.image = req.body.image;
-    res.send(product);
-})
+router.put("/:id", function (req, res) {
+  let id = req.params.id;
+  let product = products.find((product) => product.id === id);
+  product.name = req.body.name;
+  product.price = req.body.price;
+  product.description = req.body.description;
+  product.image = req.body.image;
+  res.send(product);
+});
 /* DELETE Eliminar un producto */
-router.delete('/:id', function(req, res) {
-    let id = req.params.id;
-    let product = products.find(product => product.id === id);
-    products.splice(products.indexOf(product), 1);
-    res.send(product);
-})
+router.delete("/:id", function (req, res) {
+  let id = req.params.id;
+  let product = products.find((product) => product.id === id);
+  products.splice(products.indexOf(product), 1);
+  res.send(product);
+});
 
 module.exports = router;
