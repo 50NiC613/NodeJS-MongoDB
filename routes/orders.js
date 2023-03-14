e0cbbfe0d773357cb9a77b4cab2fc4a4af31718b
50NiC613 <yulianmunoz157@gmail.com>
2023-03-13 23:38:33 -0400

    Rest Client configurado

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Order = require("../models/order");
+const Order = require("../models/orders");
 /* GET Listar las órdenes */
 router.get("/", function (req, res) {
   res.send("respond with a resource");
@@ -26,6 +26,19 @@ router.post("/", async function (req, res) {
         error: err,
       });
     });
+});
+/**
+ * PUT Actualizar una orden async
+ */
+router.put("/:id", async (req, res) => {
+  const order = await Order.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  if (order) {
+    res.status(200).json({ message: "Orden actualizada", order: order });
+  } else {
+    res.status(500).json({ message: "No se encontro esa orden" });
+  }
 });
 
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const { isValidObjectId } = require("mongoose");
 const router = express.Router();
 const { Product } = require("../models/product");
 
-/* GET Listar los productos */
-router.get("/", async function (req, res) {
+/* GET Listar los productos
+ */
+
+router.get("/", async (req, res) => {
   let products = await Product.find();
   if (!products.length) {
     res.status(500).json({ success: false });
@@ -11,22 +14,41 @@ router.get("/", async function (req, res) {
   res.send(products);
 });
 /* GET producto por ID */
-router.get("/:id", function (req, res) {
-  let id = req.params.id;
-  let product = products.find((product) => product.id === id);
-  res.send(product);
+router.get("/:id", async (req, res) => {
+  // validar que es valido el id
+  let id = null;
+  if (isValidObjectId(req.params.id)) {
+    id = req.params.id;
+  }
+  let product = await Product.findById(id);
+  if (product) {
+    res.status(200).json({
+      message: "Se encontro el producto",
+      product: product,
+    });
+  } else {
+    res.status(500).json({ message: "No se encontro el producto" });
+  }
 });
 // importar modelo de producto
 
-/* POST Crear un nuevo producto */
-router.post("/", function (req, res) {
+/* POST Crear un nuevo producto del modelo Product*/
+router.post("/", async (req, res) => {
   let product = new Product({
     name: req.body.name,
     price: req.body.price,
     description: req.body.description,
     image: req.body.image,
+    images: req.body.images,
+    brand: req.body.brand,
+    category: req.body.category,
+    countInStock: req.body.countInStock,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+    isFeatured: req.body.isFeatured,
+    dateCreated: req.body.dateCreated,
   });
-  product
+  await product
     .save(product)
     .then(() => {
       res.status(200).json({
@@ -41,6 +63,7 @@ router.post("/", function (req, res) {
       });
     });
 });
+
 /* PUT Actualizar un producto */
 router.put("/:id", function (req, res) {
   let id = req.params.id;
@@ -52,7 +75,7 @@ router.put("/:id", function (req, res) {
   res.send(product);
 });
 /* DELETE Eliminar un producto */
-router.delete("/:id", async function (req, res) {
+router.delete("/:id", async (req, res) => {
   await Product.findByIdAndDelete(req.params.id).then((product) => {
     res
       .status(200)
